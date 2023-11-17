364ecf4f5c56dc4ee06a6610453f8e69c95a738d
Zehraulker07 <151157467+Zehraulker07@users.noreply.github.com>
2023-11-17 17:14:52 +0300

    Update AndroidEventConfig.kt

--- app/src/main/java/com/accesskit/androideventapp/AndroidEventConfig.kt
+++ app/src/main/java/com/accesskit/androideventapp/AndroidEventConfig.kt
@@ -41,7 +41,13 @@ object AndroidEventConfig {
         // Participant 9
         // Participant 10
         // Participant 11
-        // Participant 12
+        Participant(
+            profileImageUrl = "https://i.hizliresim.com/f91y7wq.jpg",
+            name = "Zehra",
+            surname = "ULKER",
+            emoji = "🫶",
+            imageUrl = "https://i.hizliresim.com/9kigjxr.jpg"
+        ),
         // Participant 13
         // Participant 14
         // Participant 15
